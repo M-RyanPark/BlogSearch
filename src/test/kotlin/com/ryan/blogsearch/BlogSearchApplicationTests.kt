5ea7f0737dcbfe03065025f4618b64b1eb61e18e
sh-ryan-park <sh.rayn.park@dreamus.io>
2022-09-18 17:16:39 +0900

    Spring initializer 및 gradle 설정

---
+++ src/test/kotlin/com/ryan/blogsearch/BlogSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ryan.blogsearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogSearchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
